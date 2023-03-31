ce4ee9d2dae264625f79f1c70d39e0bb4b378eb3
Geoffrey Ameble <56687199+Aceofthehood1@users.noreply.github.com>
2023-03-31 12:41:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".auto-type",{
+    strings: ["Hello, My Name is Geoffrey Ameble","Welcome to my portfolio homepage"] ,
+    typeSpeed: 100 ,
+    backSpeed: 50,
+    looped: true
+
+})
